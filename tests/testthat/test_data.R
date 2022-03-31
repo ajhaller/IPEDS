de92c074c3aa234e0979ee1790826b35b32c42a3
Aushanae <ahaller@smith.edu>
2022-03-30 22:53:48 -0400

    more tests

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -29,9 +29,47 @@ test_that("number of full time enrollment combined with part time enrollment equ
     na.omit(FT_enroll) %>%
     na.omit(PT_enroll) %>%
     na.omit(ENRLT)
-  expect_equal(sum(adm_2020_enrl$FT_enroll + adm_2020_enrl$PT_enroll, adm_2020_enrl$ENRLT))
+  expect_equal(sum(adm_2020_enrl$FT_enroll + adm_2020_enrl$PT_enroll), sum(adm_2020_enrl$ENRLT))
 
 })
 
+test_that("the possible range of Reading-Writing SAT scores 25th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$RW_SAT_25, na.rm = TRUE) >= 200 & max(adm2020$RW_SAT_25, na.rm = TRUE) <= 800)
+})
 
+test_that("the possible range of Reading-Writing SAT scores 75th percentilein Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$RW_SAT_75, na.rm = TRUE) >= 200 & max(adm2020$RW_SAT_75, na.rm = TRUE) <= 800)
+})
+
+test_that("the possible range of Math SAT scores 25th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$MTH_SAT_25, na.rm = TRUE) >= 200 & max(adm2020$MTH_SAT_25, na.rm = TRUE) <= 800)
+})
+
+test_that("the possible range of Math SAT scores 75th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$MTH_SAT_75, na.rm = TRUE) >= 200 & max(adm2020$MTH_SAT_75, na.rm = TRUE) <= 800)
+})
+
+test_that("the possible range of ACT composite scores 25th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$ACT_25, na.rm = TRUE) >= 1 & max(adm2020$ACT_25, na.rm = TRUE) <= 36)
+})
+
+test_that("the possible range of ACT composite scores 75th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$ACT_75, na.rm = TRUE) >= 1 & max(adm2020$ACT_75, na.rm = TRUE) <= 36)
+})
+
+test_that("the possible range of ACT English scores 25th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$eng_ACT_25, na.rm = TRUE) >= 1 & max(adm2020$eng_ACT_25, na.rm = TRUE) <= 36)
+})
+
+test_that("the possible range of ACT English scores 75th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$eng_ACT_75, na.rm = TRUE) >= 1 & max(adm2020$eng_ACT_75, na.rm = TRUE) <= 36)
+})
+
+test_that("the possible range of ACT Math scores 25th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$MTH_ACT_25, na.rm = TRUE) >= 1 & max(adm2020$MTH_ACT_25, na.rm = TRUE) <= 36)
+})
+
+test_that("the possible range of ACT Math scores 75th percentile in Admissions Fall 2020 df is correct", {
+  expect_true(min(adm2020$MTH_ACT_75, na.rm = TRUE) >= 1 & max(adm2020$MTH_ACT_75, na.rm = TRUE) <= 36)
+})
 
