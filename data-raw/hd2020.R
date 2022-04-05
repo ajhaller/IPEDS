b6abb548502113c63ab1ffba34866a6ddaf55574
Aushanae <ahaller@smith.edu>
2022-04-05 16:19:38 -0400

    selecting needed variables

--- data-raw/hd2020.R
+++ data-raw/hd2020.R
@@ -6,6 +6,13 @@ temp <- tempfile()
 download.file("https://nces.ed.gov/ipeds/datacenter/data/HD2020.zip", temp)
 hd2020 <- read.table(unz(temp, "hd2020.csv"), sep = ",", header = TRUE)
 
+hd2020 <- hd2020 %>%
+  select(-IALIAS, -CHFNM, -CHFTITLE, -GENTELE, -EIN, -DUNS, -OPEID, -SECTOR,
+         -HLOFFER, -DEGGRANT, -NEWID, -DEATHYR, -CYACTIVE, -POSTSEC, -PSET4FLG,
+         -INSTCAT, -C18BASIC, -C15BASIC, -CCBASIC, -LANDGRNT, -CBSA, -CBSATYPE,
+         -CSA, -NECTA, -DFRCGID, -DFRCUSCG) %>%
+  filter(CLOSEDAT == -2 & PSEFLAG == 1)
+
 unlink(temp)
 
 usethis::use_data(hd2020, overwrite = TRUE)
