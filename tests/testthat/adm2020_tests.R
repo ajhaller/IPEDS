23e18f25b72c5989bd1fdc7e8d92671d171ef229
Aushanae <ahaller@smith.edu>
2022-04-06 20:43:16 -0400

    dir info tests

--- tests/testthat/adm2020_tests.R
+++ tests/testthat/adm2020_tests.R
@@ -73,26 +73,3 @@ test_that("the possible range of ACT Math scores 25th percentile in Admissions F
 test_that("the possible range of ACT Math scores 75th percentile in Admissions Fall 2020 df is correct", {
   expect_true(min(adm2020$MTH_ACT_75, na.rm = TRUE) >= 1 & max(adm2020$MTH_ACT_75, na.rm = TRUE) <= 36)
 })
-
-#dir_info2020 tests
-
-testthat::test_that("number of columns in Directory Information 2020 df is correct", {
-  expect_equal(ncol(dir_info2020), 47)
-})
-
-
-test_that("number of rows in Directory Information 2020 df is correct", {
-  expect_equal(nrow(dir_info2020), 6220)
-})
-
-
-test_that("Number of unique institution IDs is 1989", {
-  dir_info2020_2 <- adm2020 %>%
-    dplyr::group_by(INSTITUTION_ID) %>%
-    dplyr::summarise(n = dplyr::n())
-  expect_equal(nrow(dir_info2020), 6220)
-})
-
-testthat::test_that("length of institution id in Directory Information 2020 df is correct", {
-  expect_equal(nchar(dir_info2020$INSTITUTION_ID), rep(6, 6220))
-})
