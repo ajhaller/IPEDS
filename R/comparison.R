c405218ae75103eedbce37b68fea7c9280bf2ef8
Alejandra Munoz <amunozgarcia@smith.edu>
2022-04-14 11:26:28 -0400

    import dplyr

--- R/comparison.R
+++ R/comparison.R
@@ -1,3 +1,6 @@
+
+#'@import dplyr
+
 globalVariables(c("fin_aid1920", "adm_2020", "offerings2020", "dir_info2020", "INSTITUTION", "INT_SIZE", "FT_enroll", "PT_enroll", "AVG_AID", "AVG_AWRD", "CITY", "STATE",
                   "BEA_REG", "LOCALE", "CALSYS", "adm_tscores", "R_B_PRICE", "APPLFEEU", "ACADEMIC", "AP",
                   "DUAL", "STUDY_ABROAD", "REME_SERV", "ACAREER_SERV", "EMPLOY_SERV", "PLACE_SERV", "DAYCARE", "LIVE_ONCAMP",
