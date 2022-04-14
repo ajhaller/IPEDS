560b45a4e6ed12516bb48565f74643b1acbcba1e
Alejandra Munoz <amunozgarcia@smith.edu>
2022-04-14 11:23:26 -0400

    global variables

--- R/comparison.R
+++ R/comparison.R
@@ -1,7 +1,15 @@
+globalVariables(c("fin_aid1920", "adm_2020", "offerings2020", "dir_info2020", "INSTITUTION", "INT_SIZE", "FT_enroll", "PT_enroll", "AVG_AID", "AVG_AWRD", "CITY", "STATE",
+                  "BEA_REG", "LOCALE", "CALSYS", "adm_tscores", "R_B_PRICE", "APPLFEEU", "ACADEMIC", "AP",
+                  "DUAL", "STUDY_ABROAD", "REME_SERV", "ACAREER_SERV", "EMPLOY_SERV", "PLACE_SERV", "DAYCARE", "LIVE_ONCAMP",
+                  "MEALSWK"))
+
+
+
+
 
 # combine dataframes into one
 
-c_df <- left_join(adm2020, fin_aid1920, by = "INSTITUTION_ID")
+c_df <- left_join(fin_aid1920, adm_2020, by = "INSTITUTION_ID")
 
 c_df <- left_join(c_df, offerings2020, by = "INSTITUTION_ID")
 
@@ -13,4 +21,32 @@ c_df <- c_df[!duplicated(c_df$INSTITUTION_ID),]
 # select the variables i want to keep - will probs take some out
 
 c_df <- c_df %>%
-  select("INT_SIZE", "INSTITUTION_ID", "FT_enroll", "PT_enroll", "AVG_AID", "AVG_AWRD", "AVG_FLOAN", "CITY", "STATE", "HBCU", "TRIBAL", "LOCALE", "OPENPUBL","adm_tscores", "TUITVARY", "R_B_PRICE", "APPLFEEU", "ACADEMIC", "AP", "DUAL", "STUDY_ABROAD", "REME_SERV", "ACAREER_SERV", "EMPLOY_SERV", "PLACE_SERV", "DAYCARE", "LIVE_ONCAMP", "MEALSWK", "NCAA")
+  select("INSTITUTION", "INT_SIZE", "FT_enroll", "PT_enroll", "AVG_AID", "AVG_AWRD", "CITY", "STATE",
+         "BEA_REG", "LOCALE", "CALSYS", "adm_tscores", "R_B_PRICE", "APPLFEEU", "ACADEMIC", "AP",
+         "DUAL", "STUDY_ABROAD", "REME_SERV", "ACAREER_SERV", "EMPLOY_SERV", "PLACE_SERV", "DAYCARE", "LIVE_ONCAMP",
+         "MEALSWK"
+         )
+
+c_df <- c_df %>%
+  rename(
+    "Institution" = "INSTITUTION",
+    "Size" = "INT_SIZE",
+    "Full Time Students" = "FT_enroll",
+    "Part Time Students" = "PT_enroll",
+    "Average Aid Awarded" = "AVG_AID",
+    "Average Award Size" = "AVG_AWRD",
+    "City" = "CITY",
+    "State" = "STATE",
+    "Region" = "BEA_REG",
+    "Urbanization" = "LOCALE",
+    "Calender System" = "CALSYS",
+    "Admission Test Scores" = "adm_tscores",
+    "Room & Board Cost" = "R_B_PRICE",
+    "Application Fee" = "APPLFEEU",
+    "Degrees Offered" = "ACADEMIC",
+    "AP Credit Accepted" = "AP",
+    "Dual Enrollment Credit Accepted" = "DUAL",
+    "Study Abroad Programs" = "STUDY_ABROAD",
+    "Meals per Week" = "MEALSWK"
+
+  )
