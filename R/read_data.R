cb6e6f66d4ec5e9b7c0fddbe4270d4948e62e02e
Aushanae <ahaller@smith.edu>
2022-04-19 15:48:28 -0400

    fix fun

--- R/read_data.R
+++ R/read_data.R
@@ -1,4 +1,4 @@
-globalVariables(c("dataset", "url", "temp", "out"))
+globalVariables(c("dataset", "url", "temp", "out", "IC2020", "HD2020"))
 
 #' Function for importing raw data from IPEDS
 #'
@@ -7,9 +7,10 @@ globalVariables(c("dataset", "url", "temp", "out"))
 #' @param dataset (character) abbrevation of dataset from IPEDS. For the list of all possible datasets to import, visit the [IPEDS datacenter](https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx?year=-1) and refer to the `Data File` column.
 #' @return A dataframe with desired information
 #' @examples
-#' read_data(IC2020)
-#' read_data(HD2020)
+#' read_data("IC2020")
+#' read_data("HD2020")
 #' @import readr
+#' @importFrom utils download.file
 #' @export
 read_data <- function(dataset) {
   url <- paste0("https://nces.ed.gov/ipeds/datacenter/data/", dataset, ".zip")
