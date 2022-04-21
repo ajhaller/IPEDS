5f22f1078f0dd87619d265baa6f9333c9324fe7e
Aushanae <ahaller@smith.edu>
2022-04-21 14:33:25 -0400

    edit

--- data-raw/Sticker/sticker.R
+++ data-raw/Sticker/sticker.R
@@ -8,4 +8,4 @@ font_add_google("Gugi", "Gugi")
 
 chart <- image_read("~/Documents/Smith College/SDS 270/Div 3/IPEDS/data-raw/Sticker/IPEDS-New.png")
 sticker(chart, package = "IPEDS", p_color = "#65350F", p_family = "Gugi", h_color="#65350F", h_fill = "#ecd4b4",
-filename="hex_IPEDSII.png", p_size=3.6, s_x=1, s_y=1, s_width=1.6, s_height = 1.6, p_y = 1)
+filename="hex_IPEDSII.png", p_size=3.6, s_x=1, s_y=1, s_width=1.8, s_height = 1.8, p_y = 1)
