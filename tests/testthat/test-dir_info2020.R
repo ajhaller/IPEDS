870d58e563564852f49d5e288ecdff3a025f7523
ajhaller <ahaller@smith.edu>
2022-11-09 00:04:37 -0500

    Updated dir_info2020 test

--- tests/testthat/test-dir_info2020.R
+++ tests/testthat/test-dir_info2020.R
@@ -14,7 +14,7 @@ test_that("number of rows in Directory Information 2020 df is correct", {
 
 
 test_that("Number of unique institution IDs is 1989", {
-  dir_info2020_2 <- adm2020 %>%
+  dir_info2020_2 <- dir_info2020 %>%
     dplyr::group_by(INSTITUTION_ID) %>%
     dplyr::summarise(n = dplyr::n())
   expect_equal(nrow(dir_info2020), 6220)
